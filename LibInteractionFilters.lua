98a654ed7ad21a0c93fa0aec01497baddd04508a
IsJustaGhost <56104601+IsJustaGhost@users.noreply.github.com>
2023-06-13 05:11:11 -0700

    Changed "filter" to "type" in most placed.

--- LibInteractionFilters.lua
+++ LibInteractionFilters.lua
@@ -1,3 +1,10 @@
+--[[
+	TODO:
+	Look into making a way to be able to call 
+
+	LIB_IF_ACTIONTYPE_ANY ?
+]]
+
 
 local LIB_IDENTIFIER, LIB_VERSION = "LibInteractionFilters", 03
 if _G[LIB_IDENTIFIER] and _G[LIB_IDENTIFIER].version > LIB_VERSION then
@@ -9,10 +16,11 @@ lib.name = LIB_IDENTIFIER
 lib.version = LIB_VERSION
 _G[LIB_IDENTIFIER] = lib
 
+local ANY_ACTION = 'Any Action'
 -----------------------------------------------------------------------------
--- actionFilters
+-- actionTypes
 -----------------------------------------------------------------------------
-local actionFilters = {
+local actionTypes = {
 	'LIB_IF_ACTIONTYPE_SEARCH',		-- Search - loot
 	'LIB_IF_ACTIONTYPE_TALK',		-- Talk
 	'LIB_IF_ACTIONTYPE_HARVEST',	-- Harvest (Mine, Cut, Collect)
@@ -47,10 +55,10 @@ local actionFilters = {
 
 }
 
--- "Registring" the actionFilters to global
-for i, actionFilter in ipairs(actionFilters) do
-	if actionFilter ~= '' then
-		_G[actionFilter] = i
+-- "Registring" the actionTypes to global
+for i, actionType in ipairs(actionTypes) do
+	if actionType ~= '' then
+		_G[actionType] = i
 	end
 end
 
@@ -67,14 +75,13 @@ end
 -----------------------------------------------------------------------------
 local lib_reticle = RETICLE
 
-function lib:IterateOverActions(action)
-	local actionFilters = self.actionFilters[action] or {}
-	local nextKey, nextData = next(actionFilters)
-	checkAll = true
+function lib.IterateSelectedAction(action)
+	local actionTypes = lib.actionTypes[action] or {}
+	local nextKey, nextData = next(actionTypes)
 	return function()
 		while nextKey do
 			local registerdName, filterFunction = nextKey, nextData
-			nextKey, nextData = next(actionFilters, nextKey)
+			nextKey, nextData = next(actionTypes, nextKey)
 			
 			if filterFunction then
 				return filterFunction
@@ -84,11 +91,11 @@ function lib:IterateOverActions(action)
 end
 
 -- where ... = interactableName, interactionPossible, currentFrameTimeSeconds
-function lib:IsInteractionDisabled(action, ...)
+function lib.IsInteractionDisabled(action, ...)
 	local isDisabled = false
 
-	for k, _action in ipairs({'all', action}) do
-		for filterFunction in self:IterateOverActions(_action) do
+	for k, _action in ipairs({ANY_ACTION, action}) do
+		for filterFunction in lib.IterateSelectedAction(_action) do
 			if filterFunction(action, ...) then
 				isDisabled = true
 			end
@@ -99,77 +106,80 @@ function lib:IsInteractionDisabled(action, ...)
 end
 
 -- where ... = interactionPossible, currentFrameTimeSeconds
-function lib:OnTryHandelingInteraction(...)
+function lib.OnTryHandelingInteraction(...)
 	local action, interactableName = GetGameCameraInteractableActionInfo()
 
-	self.interactionDisabled = self:IsInteractionDisabled(action, interactableName, ...)
-	if self.interactionDisabled then
+	lib.interactionDisabled = lib.IsInteractionDisabled(action, interactableName, ...)
+	if lib.interactionDisabled then
 		-- set the interaction as blocked, to gray out the keybind
-		lib_reticle.interactionBlocked = self.interactionDisabled
+		lib_reticle.interactionBlocked = lib.interactionDisabled
 	end
 end
 
 -----------------------------------------------------------------------------
 -- Un/Register
 -----------------------------------------------------------------------------
-function lib:RegisterForOnTryHandlingInteractionCallback(registerdName, action, filter)
-	if type(action) == 'function' then
-		filter = action
-		action = 'all'
-	end
-	if type(action) == 'number' then
+function lib.RegisterOnTryHandlingInteraction(registerdName, action, callback)
+	local action_type = type(action)
+	if action_type == 'function' then
+		callback = action
+		action = ANY_ACTION
+	elseif action_type == 'number' then
 		action = GetString(action)
 	end
 	
-	local actionFilters = self.actionFilters or {}
-	self.actionFilters = actionFilters
-	local actionFilter = actionFilters[action] or {}
-	actionFilters[action] = actionFilter
+	assert(type(registerdName) == 'string' or (callback ~= nil and type(callback) == 'function') or type(action) ~= nil , 
+		string.format("LIB_INTERACTION_FILTERS.RegisterOnTryHandlingInteraction': Your parameters are wrong. Needed types are: ... / Your values are: addonName %q, action %q, filter: %s", tostring(registerdName), tostring(action), tostring(callback)))
+	
+	local actionTypes = lib.actionTypes or {}
+	lib.actionTypes = actionTypes
+	local actionType = actionTypes[action] or {}
+	actionTypes[action] = actionType
 	
 	-- where ... = action, interactableName, interactionPossible, currentFrameTimeSeconds
-	actionFilter[registerdName] = function(...)
-		return filter(...)
+	actionType[registerdName] = function(...)
+		return callback(...)
 	end
 end
 
-function lib:UnregisterForOnTryHandlingInteractionCallback(registerdName, action)
+function lib.UnregisterOnTryHandlingInteraction(registerdName, action)
 	if type(action) == 'number' then
 		action = GetString(action)
 	else
-		action = action or 'all'
+		action = action or ANY_ACTION
 	end
 	
-	local actionFilters = self.actionFilters or {}
-	self.actionFilters = actionFilters
-	local actionFilter = actionFilters[action] or {}
-	actionFilters[action] = actionFilter
-	actionFilter[registerdName] = nil
+	local actionTypes = lib.actionTypes or {}
+	lib.actionTypes = actionTypes
+	local actionType = actionTypes[action] or {}
+	actionTypes[action] = actionType
+	actionType[registerdName] = nil
 	
-	if NonContiguousCount(actionFilters[action]) == 0 then
-		self.actionFilters[action] = nil
+	if NonContiguousCount(actionTypes[action]) == 0 then
+		lib.actionTypes[action] = nil
 	end
 end
 
 -----------------------------------------------------------------------------
 -- Get action info
 -----------------------------------------------------------------------------
--- Post the list of actions and thier actionFilter to chat
-function lib:ListAllActionsInfo()
-	for i, actionFilter in ipairs(actionFilters) do
-		if actionFilter ~= '' then
-			d( zo_strformat('<<1>> = <<2>>, String = <<3>>', actionFilter, i, GetString('SI_LIB_IF_GAMECAMERAACTIONTYPE', i)))
+-- Post the list of actions and thier actionType to chat
+function lib.ListAllActionsInfo()
+	for i, actionType in ipairs(actionTypes) do
+		if actionType ~= '' then
+			d( zo_strformat('<<1>> = <<2>>, String = <<3>>', actionType, i, GetString('SI_LIB_IF_GAMECAMERAACTIONTYPE', i)))
 		end
 	end
 end
 
 -- returns [table]actionsInfo
-function lib:GetAllActionsInfo()
+function lib.GetAllActionsInfo()
 	local actionsInfo = {}
-	for i, actionFilter in ipairs(actionFilters) do
-		if actionFilter ~= '' then
+	for i, actionType in ipairs(actionTypes) do
+		if actionType ~= '' then
 			local newEntry = {
-					actionFilter = i,
-					constant = actionFilter,
+					actionType = i,
+					constant = actionType,
 					action = GetString('SI_LIB_IF_GAMECAMERAACTIONTYPE', i)
 				}
 			table.insert(actionsInfo, newEntry)
@@ -179,31 +189,39 @@ function lib:GetAllActionsInfo()
 end
 
 LIB_INTERACTION_FILTERS = lib
---	/script LIB_INTERACTION_FILTERS:ListAllActionsInfo()
---	/tb LIB_INTERACTION_FILTERS:GetAllActionsInfo()
+--	/script LIB_INTERACTION_FILTERS.ListAllActionsInfo()
+--	/tb LIB_INTERACTION_FILTERS.GetAllActionsInfo()
 -----------------------------------------------------------------------------
 -- Hook
 -----------------------------------------------------------------------------
--- RETICLE.interactionDisabled is added and used to prevent interaction at INTERACTIVE_WHEEL_MANAGER:StartInteraction()
+-- RETICLE.interactionDisabled is added and used to prevent interaction at INTERACTIVE_WHEEL_MANAGER.StartInteraction()
 -- Setting RETICLE.interactionBlocked to true will cause the reticle keybind to turn gray.
 SecurePostHook(lib_reticle, "TryHandlingInteraction", function(self, interactionPossible, currentFrameTimeSeconds)
 	if not interactionPossible then return end
 	
-	lib:OnTryHandelingInteraction(interactionPossible, currentFrameTimeSeconds)
+	lib.OnTryHandelingInteraction(interactionPossible, currentFrameTimeSeconds)
 end)
 
 -----------------------------------------------------------------------------
 -- Disabling interaction.
 -----------------------------------------------------------------------------
+-- This must be used in order to allow gamepad users to jump while interation is disabled
+function lib_reticle:GetInteractPromptVisible()
+	if self.interactionDisabled then
+		return false
+	end
+	return not self.interact:IsHidden()
+end
+
 -- This must return true in order to prevent interactions
 -- Using a hook would return nil, which would allow the interaction to run.
 -- if not INTERACTIVE_WHEEL_MANAGER:StartInteraction(ZO_INTERACTIVE_WHEEL_TYPE_FISHING) then GameCameraInteractStart() end
 local orig_StartInteraction = INTERACTIVE_WHEEL_MANAGER.StartInteraction
 function INTERACTIVE_WHEEL_MANAGER:StartInteraction(interactiveWheelType)
 	-- Lets at least run the original so it can do it's thing before canceling it out to disable interactions.
-	local result = orig_StartInteraction(self, interactiveWheelType)
 	if interactiveWheelType == ZO_INTERACTIVE_WHEEL_TYPE_FISHING and lib.interactionDisabled then
 		return true
 	end
+	local result = orig_StartInteraction(self, interactiveWheelType)
 	return result
 end
