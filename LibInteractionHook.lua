ddfc5ceea32dde32d2bfe2606d9a8c0d22a694ed
IsJustaGhost <56104601+IsJustaGhost@users.noreply.github.com>
2023-06-13 21:27:39 -0700

    Changed hooked to allow hiding the interaction

--- LibInteractionHook.lua
+++ LibInteractionHook.lua
@@ -22,6 +22,8 @@ lib.version = LIB_VERSION
 _G[LIB_IDENTIFIER] = lib
 
 local ANY_ACTION = 'Any Action'
+local lib_actionTypes = {}
+lib.actionTypes = lib_actionTypes
 -----------------------------------------------------------------------------
 -- actionTypes
 -----------------------------------------------------------------------------
@@ -81,7 +83,7 @@ end
 local lib_reticle = RETICLE
 
 function lib.IterateSelectedAction(action)
-	local actionTypes = lib.actionTypes[action] or {}
+	local actionTypes = lib_actionTypes[action] or {}
 	local nextKey, nextData = next(actionTypes)
 	return function()
 		while nextKey do
@@ -95,11 +97,11 @@ function lib.IterateSelectedAction(action)
 	end
 end
 
--- where ... = interactableName, interactionPossible, currentFrameTimeSeconds
+-- where ... = interactableName, currentFrameTimeSeconds
 function lib.IsInteractionDisabled(action, ...)
 	local isDisabled = false
 
-	for k, _action in ipairs({ANY_ACTION, action}) do
+	for k, _action in ipairs({action, ANY_ACTION}) do
 		for callback in lib.IterateSelectedAction(_action) do
 			if callback(action, ...) then
 				isDisabled = true
@@ -110,7 +112,7 @@ function lib.IsInteractionDisabled(action, ...)
 	return isDisabled
 end
 
--- where ... = interactionPossible, currentFrameTimeSeconds
+-- where ... = currentFrameTimeSeconds
 function lib.OnTryHandelingInteraction(...)
 	local action, interactableName = GetGameCameraInteractableActionInfo()
 
@@ -121,10 +123,15 @@ function lib.OnTryHandelingInteraction(...)
 	end
 end
 
+function lib:HideInteraction(hidden)
+	lib_reticle.interact:SetHidden(true)
+end
+-- lib_reticle:RequestHidden(true)
+-- lib_reticle:RequestHidden(false)
 -----------------------------------------------------------------------------
 -- Un/Register
 -----------------------------------------------------------------------------
-function lib.RegisterOnTryHandlingInteraction(registerdName, action, callback)
+function lib:RegisterOnTryHandlingInteraction(registerdName, action, callback)
 	local action_type = type(action)
 	if action_type == 'function' then
 		callback = action
@@ -136,32 +143,29 @@ function lib.RegisterOnTryHandlingInteraction(registerdName, action, callback)
 	assert(type(registerdName) == 'string' or (callback ~= nil and type(callback) == 'function') or type(action) ~= nil , 
 		string.format("LIB_INTERACTION_FILTERS.RegisterOnTryHandlingInteraction': Your parameters are wrong. Needed types are: ... / Your values are: addonName %q, action %q, filter: %s", tostring(registerdName), tostring(action), tostring(callback)))
 	
-	local actionTypes = lib.actionTypes or {}
-	lib.actionTypes = actionTypes
-	local actionType = actionTypes[action] or {}
-	actionTypes[action] = actionType
+	local actionType = lib_actionTypes[action] or {}
+	lib_actionTypes[action] = actionType
 	
-	-- where ... = action, interactableName, interactionPossible, currentFrameTimeSeconds
+	-- where ... = action, interactableName, currentFrameTimeSeconds
 	actionType[registerdName] = function(...)
 		return callback(...)
 	end
 end
 
-function lib.UnregisterOnTryHandlingInteraction(registerdName, action)
+function lib:UnregisterOnTryHandlingInteraction(registerdName, action)
 	if type(action) == 'number' then
 		action = GetString(action)
 	else
 		action = action or ANY_ACTION
 	end
 	
-	local actionTypes = lib.actionTypes or {}
-	lib.actionTypes = actionTypes
-	local actionType = actionTypes[action] or {}
-	actionTypes[action] = actionType
-	actionType[registerdName] = nil
-	
-	if NonContiguousCount(actionTypes[action]) == 0 then
-		lib.actionTypes[action] = nil
+	local actionType = lib_actionTypes[action]
+	if actionType ~= nil then
+		actionType[registerdName] = nil
+		
+		if NonContiguousCount(lib_actionTypes[action]) == 0 then
+			lib_actionTypes[action] = nil
+		end
 	end
 end
 
@@ -196,17 +200,36 @@ end
 LIB_INTERACTION_HOOK = lib
 --	/script LIB_INTERACTION_HOOK.ListAllActionsInfo()
 --	/tb LIB_INTERACTION_HOOK.GetAllActionsInfo()
+--	/tb LIB_INTERACTION_HOOK:GetAllActionsInfo()
 -----------------------------------------------------------------------------
 -- Hook
 -----------------------------------------------------------------------------
 -- RETICLE.interactionDisabled is added and used to prevent interaction at INTERACTIVE_WHEEL_MANAGER.StartInteraction()
 -- Setting RETICLE.interactionBlocked to true will cause the reticle keybind to turn gray.
+--SecurePostHook(lib_reticle, "TryHandlingInteraction", function(self, currentFrameTimeSeconds)
+
+-- Changing it to use "UpdateInteractText" to make it so that hiding the 
+-- interaction is possible with LIB_INTERACTION_HOOK:HideInteraction()
+--[[
+	
 SecurePostHook(lib_reticle, "TryHandlingInteraction", function(self, interactionPossible, currentFrameTimeSeconds)
 	if not interactionPossible then return end
 	
-	lib.OnTryHandelingInteraction(interactionPossible, currentFrameTimeSeconds)
+	lib.OnTryHandelingInteraction( currentFrameTimeSeconds)
+end)
+]]
+SecurePostHook(lib_reticle, "UpdateInteractText", function(self, currentFrameTimeSeconds)
+    local interactionPossible = not self.interact:IsHidden()
+	if IsPlayerGroundTargeting() then return end
+
+	local interactionExists, _, questInteraction = GetGameCameraInteractableInfo()
+	if interactionExists and not questInteraction then
+		lib.OnTryHandelingInteraction(currentFrameTimeSeconds)
+	end
 end)
 
+-- TryHandlingNonInteractableFixture
+-- TryHandlingQuestInteraction
 -----------------------------------------------------------------------------
 -- Disabling interaction.
 -----------------------------------------------------------------------------
